44a11ef56aa398c777e3d5d5a80c402b80409127
Mark Burns <markburns@gmail.com>
2021-01-12 23:58:27 +0000

    homework for week 1 day 2 added

---
+++ week_01/day_02/homework_result/my_functions.py
@@ -0,0 +1,73 @@
+# def greet():
+#     print("Hey")
+
+# greet()
+
+#---------------------------------------------------------#
+
+# def greet():
+#     return "Hey"
+
+# greeting = greet()
+# print(greeting)
+
+#---------------------------------------------------------#
+
+# def greet():
+#     print("Hey")
+
+# greeting = greet()
+# print(greeting)
+
+#---------------------------------------------------------#
+
+# def greet(name):
+#     return "Hey " + name
+
+# greeting = greet("Mark")
+# print(greeting)
+
+#---------------------------------------------------------#
+
+# def greet(name, time_of_day):
+#     return "Good " + time_of_day + ", " + name
+
+# greeting = greet("Mark", "morning")
+# print(greeting)
+
+#---------------------------------------------------------#
+
+# def wash_clothes(laundry, detergent, water):
+#     clean_laundry = laundry + detergent + water
+#     return clean_laundry
+
+# clean_tshirt = wash_clothes("dirty_tshirt", "liquid detergent", "water")
+# print(clean_tshirt)
+
+## Not sure what the out come on this was supposed to be
+
+#---------------------------------------------------------#
+
+# def greet(name, time_of_day):
+#     return "Good " + time_of_day + ", " + name
+
+# name_1 = "Bob"
+# time_of_day_1 = "morning"
+# greeting1 = greet(name_1, time_of_day_1)
+# print(greeting1)
+
+# name_2 = "Ada"
+# time_of_day_2 = "afternoon"
+# greeting2 = greet(name_2, time_of_day_2)
+# print(greeting2)
+
+#---------------------------------------------------------#
+
+# def greet():
+#     words = "Hey"
+#     return words
+
+# def greet_two():
+#     return words
+
+# print(greet_two())
