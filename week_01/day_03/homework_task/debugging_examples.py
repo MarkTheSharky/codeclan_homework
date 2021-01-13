3ea6c6f6f7648dd396269d81f06491691b541188
Mark Burns <markburns@gmail.com>
2021-01-13 22:16:36 +0000

    homework week 1 day 3

---
+++ week_01/day_03/homework_task/debugging_examples.py
@@ -0,0 +1,46 @@
+# What does cmd alt L do??
+
+def manipulate(num):
+    num += 10
+    print(num)
+    num /= 2
+    print(num)
+
+manipulate(20)
+
+#--------------------------------------------#
+
+def defined_method():
+    print("Success")
+
+defined_method()
+
+#--------------------------------------------#
+
+def add(num1, num2, num3):
+    return num1 + num2 + num3
+
+add(2, 4, 7)
+
+#--------------------------------------------#
+
+fruits = ["apple", "orange", "pear", "plums"]
+
+def missing(array):
+    for fruit in array:
+        if(fruit == "grape"):
+            print("Plums!!")
+        if(fruit == "apple" or fruit == "orange" or fruit == "pear"):
+            print("Meh")
+
+missing(fruits)
+
+#--------------------------------------------#
+
+
+
+#--------------------------------------------#
+
+
+
+#--------------------------------------------#
