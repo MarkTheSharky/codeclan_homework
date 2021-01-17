2c720436f4bde5611f9d1b2949ca107dcd7b28ef
Mark Burns <markburns@gmail.com>
2021-01-17 17:16:37 +0000

    3 functions complete

---
+++ week_01/weekend_homework/scrap_pad.py
@@ -0,0 +1,5 @@
+number1 = 0
+number2 = 2
+
+def add_numbers(num1, num2):
+    
--- week_01/weekend_homework/start_point/src/pet_shop.py
+++ week_01/weekend_homework/start_point/src/pet_shop.py
@@ -1,6 +1,20 @@
-#  def test_pet_shop_name(self):
-#         name = get_pet_shop_name(self.cc_pet_shop)
-#         self.assertEqual("Camelot of Pets", name)
 
-def pet_shop_name():
-    
+
+def get_pet_shop_name(pet_shop):
+    name = pet_shop["name"]
+    return name
+
+def get_total_cash(pet_shop):
+    sum = pet_shop["admin"]["total_cash"]
+    return sum
+
+# def add_or_remove_cash(total, add):
+
+# def add_or_remove_cash(total, remove):
+
+
+def get_pets_sold(pet_shop):
+    sold = pet_shop["admin"]["pets_sold"]
+    return sold
+
+
