20a34f56e08bd877b3d7021025c0d2a3b6fed613
Mark Burns <markburns@gmail.com>
2021-01-17 22:48:18 +0000

    9 tests complete

--- week_01/weekend_homework/start_point/src/pet_shop.py
+++ week_01/weekend_homework/start_point/src/pet_shop.py
@@ -8,17 +8,15 @@ def get_total_cash(pet_shop):
     sum = pet_shop["admin"]["total_cash"]
     return sum
 
-# def add_or_remove_cash(total, add):
-
-# def add_or_remove_cash(total, remove):
+def add_or_remove_cash(pet_shop_total, add):
+    pet_shop_total["admin"]["total_cash"] += add
 
 def get_pets_sold(pet_shop):
     sold = pet_shop["admin"]["pets_sold"]
     return sold
 
-# def increase_pets_sold(pet_shop, pets_sold):
-#     sold = pet_shop["admin"]["pets_sold"]
-#     sold += pets_sold
+def increase_pets_sold(pet_shop, pets_sold):
+    pet_shop["admin"]["pets_sold"] += pets_sold
 
 def get_stock_count(stock_count):
     count = stock_count["pets"]
@@ -30,3 +28,9 @@ def get_pets_by_breed(pet_shop, breed):
         if breed == pets["breed"]:
             pet_breed.append(pets)
     return pet_breed
+
+# def find_pet_by_name(pet_shop, pet_name):
+#     pet_name = []
+#     for name in pet_shop["pets"]:
+#         if name == pets["pet_name"]:
+#             return pets["pet_name"]
