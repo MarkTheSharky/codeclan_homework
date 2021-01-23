10f2c086e1208e16c5490c3ccda5d4173ab87c73
Mark Burns <markburns@gmail.com>
2021-01-23 00:33:14 +0000

    End of night commit, another test added

--- week_02/weekend_homework/karaoke_bar/src/karaoke_room.py
+++ week_02/weekend_homework/karaoke_bar/src/karaoke_room.py
@@ -3,8 +3,13 @@ class KaraokeRoom():
         self.room_name = room
         self.capacity = capacity
         self.room_list = []
+        self.assigned_room = []
 
     def add_room(self, room, capacity):
         self.room_list.append({"room_name": room, "room_capacity": capacity})
 
-    
+    def check_rooms_available(self):
+        return len(self.room_list)
+        
+    # def check_capacity(self):
+    #     room_capacity = 
--- week_02/weekend_homework/karaoke_bar/tests/karaoke_room_test.py
+++ week_02/weekend_homework/karaoke_bar/tests/karaoke_room_test.py
@@ -1,12 +1,15 @@
 import unittest
 
 from src.karaoke_room import *
+# from src.guest import *
 
 class KaraokeTest(unittest.TestCase):
     def setUp(self):
         self.room = KaraokeRoom("The Hockey Cokey Karaoke", 10)
         self.room_list= [
-            {"room_name": "Sing Song", "room_capacity": 15}
+            {"room_name": "Sing Song", "room_capacity": 15},
+            {"room_name": "The Prince Room", "room_capacity": 20},
+            {"room_name": "The Beatles Room", "room_capacity": 5}
         ]
 
 
@@ -17,8 +20,25 @@ class KaraokeTest(unittest.TestCase):
     def test_for_capacity(self):
         self.assertEqual(10, self.room.capacity)
 
-#Test to add room
+#Test to add room to a list
     def test_add_room(self):
-        expected_result = [{"room_name": "Sing Song", "room_capacity": 15}]
         self.room.add_room("Sing Song", 15)
+        expected_result = [{"room_name": "Sing Song", "room_capacity": 15}]
         self.assertEqual(expected_result, self.room.room_list)
+
+#Test to check how many rooms are available
+    def test_rooms_available(self):
+        self.room.check_rooms_available()
+        self.room.add_room("Sing Song", 15)
+        self.assertEqual(1, len(self.room.room_list))
+
+# #Test to check capacity of room
+#     def test_room_capacity(self):
+#         self.room.check_capacity()
+#         self.assertEqual(10, )
+
+# #Test to check in guests to a room
+#     def test_check_in_guest_to_room(self):
+#         self.room.assign_room()
+#         expected_result = ""
+#         self.assertEqual(expected_result, )
