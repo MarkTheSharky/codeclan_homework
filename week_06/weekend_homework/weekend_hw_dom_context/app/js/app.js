60e115461deea4b304b9c6958d1c1dd19dfae86c
Mark Burns <markburns@gmail.com>
2021-02-21 18:41:23 +0000

    MVP Complete

--- week_06/weekend_homework/weekend_hw_dom_context/app/js/app.js
+++ week_06/weekend_homework/weekend_hw_dom_context/app/js/app.js
@@ -1,8 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
     const newItemForm = document.querySelector('#form-skills')
     newItemForm.addEventListener('submit', handleNewItemFormSubmit)
+
+    const deleteAll = document.querySelector('#delete_all')
+    deleteAll.addEventListener('click', handleDeleteAll)
+
 })
 
-const handleNewItemFormSubmit = function(event) {
-    console.log("Test Print")
+const handleNewItemFormSubmit = function(evt) {
+    evt.preventDefault()    
+
+    const newListItem = document.createElement('li')
+    newListItem.textContent = `User has ${evt.target.skill_name.value} from the ${evt.target.skill_school.value} set of skills.`
+
+    const list = document.querySelector('#skills_list')
+    list.appendChild(newListItem)
+}
+
+const handleDeleteAll = function(evt) {
+    const listItems = document.querySelector('#skills_list')
+    listItems.textContent = ""
+    // const listItemsChild = document.querySelector('#skills_list li')
+    // listItems.removeChild(listItemsChild)
 }
