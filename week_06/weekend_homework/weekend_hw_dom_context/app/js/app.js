94694ac7f004bcfe5d8dc9412554c9b137df75bb
Mark Burns <markburns@gmail.com>
2021-02-22 22:24:12 +0000

    Week 06 Weekend Homework Changes

--- week_06/weekend_homework/weekend_hw_dom_context/app/js/app.js
+++ week_06/weekend_homework/weekend_hw_dom_context/app/js/app.js
@@ -11,11 +11,12 @@ const handleNewItemFormSubmit = function(evt) {
     evt.preventDefault()    
 
     const newListItem = document.createElement('li')
-    newListItem.textContent = `User has ${evt.target.skill_name.value} from the ${evt.target.skill_school.value} set of skills.`
-
+    newListItem.textContent = `${evt.target.skill_name.value} from the ${evt.target.skill_school.value} set of skills.`
+    
     const list = document.querySelector('#skills_list')
     list.appendChild(newListItem)
 }
+
 
 const handleDeleteAll = function(evt) {
     const listItems = document.querySelector('#skills_list')
