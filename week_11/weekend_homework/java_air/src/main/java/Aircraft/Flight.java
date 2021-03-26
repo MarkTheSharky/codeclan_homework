4970c2767b031030c7b6afc92c8e0d691c223eae
Mark Burns <markburns@gmail.com>
2021-03-26 23:26:51 +0000

    Completed flight class setup and tests

--- week_11/weekend_homework/java_air/src/main/java/Aircraft/Flight.java
+++ week_11/weekend_homework/java_air/src/main/java/Aircraft/Flight.java
@@ -27,4 +27,68 @@ public class Flight {
         this.departureAirport = departureAirport;
         this.departureTime = departureTime;
     }
+
+    public int countCabinCrewMembers() {
+        return this.cabinCrewMembers.size();
+    }
+
+    public void addCabinCrewMember(CabinCrewMember cabinCrewMember) {
+        this.cabinCrewMembers.add(cabinCrewMember);
+    }
+
+    public void removeCabinCrewMember() {
+        this.cabinCrewMembers.remove(0);
+    }
+
+    public void removeALLCabinCrewMembers() {
+        this.cabinCrewMembers.clear();
+    }
+
+    public int canCountPassengers() {
+        return this.passengers.size();
+    }
+
+    public void addPassenger(Passenger passenger) {
+        this.passengers.add(passenger);
+    }
+
+    public void removePassenger() {
+        this.passengers.remove(0);
+    }
+
+    public void removeALLPassengers() {
+        this.passengers.clear();
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(String flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getDepartureAirport() {
+        return departureAirport;
+    }
+
+    public void setDepartureAirport(String departureAirport) {
+        this.departureAirport = departureAirport;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
 }
--- week_11/weekend_homework/java_air/src/test/java/FlightTest.java
+++ week_11/weekend_homework/java_air/src/test/java/FlightTest.java
@@ -5,6 +5,9 @@ import Person.CrewRank;
 import Person.Passenger;
 import Person.Pilot;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class FlightTest {
 
@@ -20,7 +23,7 @@ public class FlightTest {
 
     @Before
     public void before() {
-        flight = new Flight(pilot, plane, "FR756", "GLA", "LDN", "16.00");
+        flight = new Flight(pilot, plane, "FR756", "GLA", "LDN", "18.00");
         pilot = new Pilot("Mark", CrewRank.CAPTAIN, "UK/PP/123456D/A");
         cabinCrewMember1 = new CabinCrewMember("David", CrewRank.FIRST_OFFICER);
         cabinCrewMember2 = new CabinCrewMember("Andrew", CrewRank.PURSER);
@@ -28,5 +31,103 @@ public class FlightTest {
         passenger1 = new Passenger("Kerr", 1);
         passenger2 = new Passenger("Donald", 2);
         passenger3 = new Passenger("Dave", 4);
+    }
+
+    @Test
+    public void canCountCabinCrewMembers() {
+        assertEquals(0, flight.countCabinCrewMembers());
+    }
+
+    @Test
+    public void canAddCabinCrewMember() {
+        flight.addCabinCrewMember(cabinCrewMember1);
+        assertEquals(1, flight.countCabinCrewMembers());
+    }
+
+    @Test //This should really be able to find and remove a specific cabin crew member.
+    public void canRemoveCabinCrewMember() {
+        flight.addCabinCrewMember(cabinCrewMember1);
+        flight.removeCabinCrewMember();
+        assertEquals(0, flight.countCabinCrewMembers());
+    }
+
+    @Test
+    public void canRemoveALLCabinCrewMembers() {
+        flight.addCabinCrewMember(cabinCrewMember1);
+        flight.addCabinCrewMember(cabinCrewMember2);
+        assertEquals(2, flight.countCabinCrewMembers());
+        flight.removeALLCabinCrewMembers();
+        assertEquals(0, flight.countCabinCrewMembers());
+    }
+
+    @Test
+    public void canCountPassengers() {
+        assertEquals(0, flight.canCountPassengers());
+    }
+
+    @Test
+    public void canAddPassenger() {
+        flight.addPassenger(passenger1);
+        assertEquals(1, flight.canCountPassengers());
+    }
+
+    @Test //This should really be able to find and remove a specific cabin crew member.
+    public void canRemovePassenger() {
+        flight.addPassenger(passenger1);
+        flight.removePassenger();
+        assertEquals(0, flight.canCountPassengers());
+    }
+
+    @Test
+    public void canRemoveALLPassengers() {
+        flight.addPassenger(passenger1);
+        flight.addPassenger(passenger2);
+        assertEquals(2, flight.canCountPassengers());
+        flight.removeALLPassengers();
+        assertEquals(0, flight.countCabinCrewMembers());
+    }
+
+    @Test
+    public void canGetFlightNumber() {
+        assertEquals("FR756", flight.getFlightNumber());
+    }
+
+    @Test
+    public void canSetFlightNumber() {
+        flight.setFlightNumber("FR666");
+        assertEquals("FR666", flight.getFlightNumber());
+    }
+
+    @Test
+    public void canGetFlightDestination() {
+        assertEquals("GLA", flight.getDestination());
+    }
+
+    @Test
+    public void canSetFlightDestination() {
+        flight.setDestination("EDI");
+        assertEquals("EDI", flight.getDestination());
+    }
+
+    @Test
+    public void canGetDepartureAirport() {
+        assertEquals("LDN", flight.getDepartureAirport());
+    }
+
+    @Test
+    public void canSetDepartureAirport() {
+        flight.setDepartureAirport("LAX");
+        assertEquals("LAX", flight.getDepartureAirport());
+    }
+
+    @Test
+    public void canGetFlightTime() {
+        assertEquals("18.00", flight.getDepartureTime());
+    }
+
+    @Test
+    public void canSetFlightTime() {
+        flight.setDepartureTime("00.00");
+        assertEquals("00.00", flight.getDepartureTime());
     }
 }
